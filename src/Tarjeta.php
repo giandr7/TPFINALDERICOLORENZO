05c47508e058dee13f7fc4de7565704ffae5bb88
GitSugar <30844900+GitSugar@users.noreply.github.com>
2017-10-24 10:33:12 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -2,4 +2,14 @@
 
 namespace TPFINALDERICOLORENZO;
 
+class Tarjeta{
+    protected $colectivo, $monto, $viajes_hechos, $Data, $plata;
+    public function credito() {
+        print("Su saldo actual es de $" . $this-> . ".\n");
+    };
+
+    public function viajes_tomados() {
+        return $this->viajes_hechos;
+    };
+}
 #Ahora veo como hago gianni, termine de estudiar grafos recien. Y todo lo que tenia se fue a la basura por no poder commitear. La prox acordate de invitarme. Besos, el chino
