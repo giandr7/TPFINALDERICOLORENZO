da601a7fe3fe7fe87872bf8cbf87715fed031351
giandr7 <32110705+giandr7@users.noreply.github.com>
2017-10-24 17:42:02 -0300

    Update Viajan2.php

--- src/Viajan2.php
+++ src/Viajan2.php
@@ -1,1 +1,17 @@
-//gegegegegegecxd
+<?php
+
+namespace TPFINALDERICOLORENZO;
+
+
+class Viaje{
+  protected $tipo, $monto, $transporte, $DT;
+    
+  public function __construct () {
+    $this->DT = time();
+    }
+  public function cobrar_viaje(){
+    
+  }
+  
+  
+}
