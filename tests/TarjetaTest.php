d05310131476021dda4115d7ffb3cbbf532e4bc9
giandr7 <32110705+giandr7@users.noreply.github.com>
2017-10-31 16:24:44 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -4,3 +4,12 @@ namespace TPFINALDERICOLORENZO;
 
 use PHPUnit\Framework\TestCase;
 
+class TarjetaTest extends TestCase
+{
+  public function testSalePlata()
+  {
+    $tarjeta = new Tarjeta();
+    salePlata_salePlata(500);
+    $this->assertEquals($Tarjeta->plata(), 652);
+  }
+}
