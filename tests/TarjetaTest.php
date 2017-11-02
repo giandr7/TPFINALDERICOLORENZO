036b2cab128dbdef1708ca5f47c135a459b2ed82
GitSugar <30844900+GitSugar@users.noreply.github.com>
2017-11-02 12:48:47 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -129,4 +129,20 @@ class TarjetaTest extends TestCase
     $tarjeta->retirarBici($bici_bici);
     $this->assertEquals($tarjeta->plata,0);
   }
+  public function testBici3()
+  {
+    $tarjeta = new Tarjeta();
+    $bici_bici = new Medium(1,"Bicicleta",0);
+    $tarjeta->salePlata_salePlata(25.5);
+    $tarjeta->retirarBici($bici_bici);
+    $tarjeta->retirarBici($bici_bici);
+    $this->assertEquals($tarjeta->plata,12.75);
+  }
+  public function testBici4()
+  {
+    $tarjeta = new Tarjeta();
+    $bici_bici = new Medium(1,"Bicicleta",0);
+    $tarjeta->retirarBici($bici_bici);
+    $this->assertEquals($tarjeta->plata,0);
+  }
 }
