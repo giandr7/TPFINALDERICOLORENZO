12b1d39702c1dfcd6d7329bc3231d9c726e00f99
GitSugar <30844900+GitSugar@users.noreply.github.com>
2017-11-02 12:36:53 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -125,7 +125,7 @@ class TarjetaTest extends TestCase
     $bici_bici = new Medium(1,"Bicicleta",0);
     $tarjeta->salePlata_salePlata(25.5);
     $tarjeta->retirarBici($bici_bici);
-    $tarjeta->Bici = ($tarjeta->Bici + (1 + (24*60*60)));
+    $bici_bici->Hora = ($tarjeta->Bici + (1 + (24*60*60)));
     $tarjeta->retirarBici($bici_bici);
     $this->assertEquals($tarjeta->plata,0);
   }
